6048bd67a868641262149751de1fd828155acd3c
HenryFBP <HenryFBP@gmail.com>
2018-10-04 19:51:32 -0500

    finish p2 of 2015/4
    also, cache result in memory because why not

--- 2015/4/adventcoins.py
+++ 2015/4/adventcoins.py
@@ -1,29 +1,41 @@
 import hashlib
+import os
+from joblib import Memory
 
-
-def valid_hash(hash: str):
-    """Are the first 5 chars all zeroes?"""
-    return (hash[0:5] == '00000')
+memory = Memory(cachedir=
+                os.path.join(os.path.dirname(__file__), '__pycache__'),
+                verbose=0)
 
 
 def generate_key(data, it=0):
     return data + str(abs(it) + 1)
 
 
-def try_generate_hash(data, max=100):
-    for i in range(0, max):
+@memory.cache
+def try_generate_hash(data, zeroes=5):
+    i = 0
+    while True:
         mod_data = generate_key(data, i)  # Generate key
-        h = hashlib.md5(mod_data.encode("utf-8"))
-        h2 = h.hexdigest()
+        h = hashlib.md5(mod_data.encode("utf-8")) # generate hash from key
+        h2 = h.hexdigest() # turn binary into ascii '1234567890ABCDEF'
 
-        print(f"{mod_data} -> {h2}")
+        print(f"{mod_data} -> {h2}") # key -> hash
 
-        if valid_hash(h2):
-            print("Success!")
-            return h2
+        if h2[0:zeroes] == ('0' * zeroes): # if it contains sufficient zeroes
+            print("Success!") # :-)
+            return (mod_data, h2)
+
+        i += 1
 
 
 with open('input', 'r') as f:
     data = f.readlines()[0]
 
-try_generate_hash(data, 999999999)
+vh1 = try_generate_hash(data, 5)
+vh2 = try_generate_hash(data, 6)
+
+print("Valid hash:")
+print(vh1)
+
+print("Valider hash:")
+print(vh2)
