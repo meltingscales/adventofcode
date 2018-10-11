d8deb37838f137af23f28cabe45392e1e3b82161
Henry Post <HenryFBP@gmail.com>
2018-10-10 20:46:50 -0500

    finish 2015/10

--- 2015/10/lookandsay.py
+++ 2015/10/lookandsay.py
@@ -1,5 +1,14 @@
+import os
 import re
+import time
 import unittest
+from functools import lru_cache
+
+from joblib import Memory
+
+memory = Memory(cachedir=
+                os.path.join(os.path.dirname(__file__), '__pycache__'),
+                verbose=0)
 
 REXP_SAME_DIGIT = r'(\d)\1*(.*)'
 
@@ -16,35 +25,56 @@ def lasonce(s: str = '3333') -> str:
     return str(len(s)) + s[0]
 
 
-def tokenize_str(s: str = '111133232166565', _bucket=None, verbose=False) -> str:
+@memory.cache
+def tokenize_str(s: str = '111133232166565', verbose=False) -> tuple:
     """
     :param s: A look-and-say string.
-    :return: The same string, but tokenized into a list.
+    :return: The same string, but tokenized into a tuple.
     """
 
-    if _bucket is None:
-        _bucket = []
+    bucket = []
 
-    if s == '':
-        return _bucket
+    while s != '':
 
-    r = re.match(REXP_SAME_DIGIT, s, )
+        r = re.match(REXP_SAME_DIGIT, s, )
 
-    first = s.replace(r.group(2), '')
-    rest = r.group(2)
+        first = s.replace(r.group(2), '')
+        rest = r.group(2)
 
-    if verbose:
-        print(f"want to tokenize '{s}'.")
+        if verbose:
+            print(f"want to tokenize '{s}'.")
 
-        print(first)
-        print(rest)
+            print(first)
+            print(rest)
 
-    _bucket.append(first)
+        bucket.append(first)
 
-    if rest == '':
-        return _bucket
-    else:
-        return tokenize_str(rest, _bucket)
+        s = rest
+
+    return tuple(bucket)
+
+
+@memory.cache
+def lookandsay(l: tuple) -> str:
+    """
+    Example:
+
+        f(['11', '777']) -> '2137'
+
+        "Two ones, three sevens."
+    """
+
+    if l == tuple():
+        return ''
+
+    ret = ""
+
+    for s in l:
+
+        if s != '':
+            ret += lasonce(s)
+
+    return ret
 
 
 class TestLASOnce(unittest.TestCase):
@@ -60,12 +90,33 @@ class TestTokenizer(unittest.TestCase):
     def testBasic(self):
         self.assertEqual(tokenize_str(''), [])
 
-        self.assertEqual(tokenize_str('0'), ['0'])
+        self.assertEqual(tokenize_str('0010'), ('00', '1', '0'))
 
-        self.assertEqual(tokenize_str('0010'), ['00', '1', '0'])
+        self.assertEqual(tokenize_str('112211'), ('11', '22', '11'))
 
-        self.assertEqual(tokenize_str('112211'), ['11', '22', '11'])
+
+class TestAll(unittest.TestCase):
+
+    def testAllOfEm(self):
+        self.assertEqual(lookandsay(tokenize_str('1111')), '41')
+
+        self.assertEqual(lookandsay(tokenize_str('13')), '1113')
+
+        self.assertEqual(lookandsay(tokenize_str('1113')), '3113')
 
 
 if __name__ == '__main__':
-    print("hi")
+
+    with open('input', 'r') as f:
+        word = f.readline().strip()
+
+    t = time.time()
+
+    for i in range(0, 51):
+        print(f"{time.time() - t} sec\n"
+              f"{i:3d}: len = {len(word)};\n"
+              f"{word[0:100]}\n"
+              f"->\n"
+              f"{lookandsay(tokenize_str(word))[0:100]}\n")
+
+        word = lookandsay(tokenize_str(word))
