5c69a7ae44183a880baddd9a7104e82fc659bf01
Henry Post <HenryFBP@gmail.com>
2021-12-24 19:51:40 -0500

    man, functional sucks, i cant redeclare variables

--- 2021/1/measure_trajectory.hs
+++ 2021/1/measure_trajectory.hs
@@ -11,23 +11,33 @@ import Text.Printf
 
 tupleLarger :: (Int, Int) -> (String)
 tupleLarger (first, second) = 
-    if  
-        first > second 
+    if
+        first > second
     then "True"
     else "False"
+
+measureTrajectory :: ([Int]) -> ([Int])
+measureTrajectory (integers) = do
+
+    let var = 0
+
+    forM_ [0..(integers-1)] $ \i -> do
+        let var = var + i
+
+    return var
+
 
 main :: IO ()
 main = do
     putStrLn "Hello, Haskell!"
 
-    contents :: [Char] <- readFile "./input"
+    contents :: String <- readFile "./input"
 
     let list :: [String] = words contents
 
     printf "first element in list: %s\n" (list !! 0)
 
-    putStrLn ("Hello World" !!! (2,7))
+    -- putStrLn ("Hello World" !!! (2,7))
+    -- printf "Is 1 larger than 2? %s\n" (tupleLarger (1, 2))
 
     putStrLn "Goodbye!"
-
-    printf "Is 1 larger than 2? %s" (tupleLarger (1, 2))
